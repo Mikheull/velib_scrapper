4a8feee63133cffb59507ee08fc4b2fd88caf985
Mikheull <mikhael.bailly1@orange.fr>
2020-08-18 19:09:42 +0200

    remove gzip compression, and reorganize folder data

--- app.js
+++ app.js
@@ -1,39 +1,30 @@
 const fs = require('fs');
 const axios = require('axios');
 const mkdirp = require('mkdirp')
-const rimraf = require("rimraf");
 const cron = require('node-cron');
-const compressing = require('compressing');
 
 /**
  * Création du dossier
  */
-const now = new Date()
-const dir = `./data/${now.getFullYear()}/${now.getMonth() + 1}/${now.getDate()}`
-const backup_dir = `./backup/${now.getFullYear()}/${now.getMonth() + 1}`
+const today = new Date()
+const month = `${today.getMonth() + 1}`.padStart(2, "0")
+const date = today.getDate()
+const hour = today.getHours() + 2
+const minute = `${today.getMinutes()}`.padStart(2, "0")
 
+const dir = `./data/${today.getFullYear()}`
 mkdirp(dir)
-mkdirp(backup_dir)
 
 
 /**
  * Sauvegarde des données
  */
-cron.schedule('*/30 * * * *', function(){
+cron.schedule('* * * * *', function(){
     axios({
         method: "get",
         url: "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&rows=1500",
         responseType: "stream"
     }).then(function (response) {
-        response.data.pipe(fs.createWriteStream(`${dir}/${now.getHours() + 2}-${now.getMinutes()}.json`));
+        response.data.pipe(fs.createWriteStream(`${dir}/${date}_${month}__${hour}_${minute}.json`));
     });
 });
-
-
-/**
- * GZip les données de la journée et suppression du dossier dans data
- */
-cron.schedule('59 23 * * *', function () { 
-    compressing.tar.compressDir(dir, `${backup_dir}/${now.getDate()}.tar`)
-    rimraf(dir, function () {});
-})
