509c56a7479c5453d8167a89554999512c1a6626
Mikheull <mikhael.bailly1@orange.fr>
2020-08-18 19:22:37 +0200

    fix Cron system

--- app.js
+++ app.js
@@ -3,28 +3,31 @@ const axios = require('axios');
 const mkdirp = require('mkdirp')
 const cron = require('node-cron');
 
-/**
- * Création du dossier
- */
-const today = new Date()
-const month = `${today.getMonth() + 1}`.padStart(2, "0")
-const date = today.getDate()
-const hour = today.getHours() + 2
-const minute = `${today.getMinutes()}`.padStart(2, "0")
-
-const dir = `./data/${today.getFullYear()}`
-mkdirp(dir)
+let today, month, date, hour, minute, dir
 
 
 /**
- * Sauvegarde des données
+ * Création du dossier et sauvegarde des données
  */
 cron.schedule('* * * * *', function(){
+
+    today = new Date()
+    month = `${today.getMonth() + 1}`.padStart(2, "0")
+    date = today.getDate()
+    hour = today.getHours() + 2
+    minute = `${today.getMinutes()}`.padStart(2, "0")
+
+    dir = `./data/${today.getFullYear()}`
+    mkdirp(dir)
+
     axios({
         method: "get",
         url: "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&rows=1500",
         responseType: "stream"
     }).then(function (response) {
+        // console.log(`Saved successfully at ${dir}/${date}_${month}__${hour}_${minute}.json`)
         response.data.pipe(fs.createWriteStream(`${dir}/${date}_${month}__${hour}_${minute}.json`));
+    }).catch(err => {
+        // console.log(err);
     });
 });
